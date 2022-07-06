d1e1ddbfc4e0716d0a3535f9b3575eba3f9d9ca8
jjimeneza <julian.jimenez@evertecinc.com>
2022-07-06 09:18:12 -0500

    Primer ejercicio

---
+++ array.py
@@ -0,0 +1,9 @@
+"""
+Ejercicio1:
+Programar una aplicación de consola que declare un Array de enteros y luego muestre en pantalla a través de un ciclo FOR cada uno de los elementos almacenados
+"""
+
+numeros = [10, 20, 30, 40, 50, 60]
+
+for x in numeros:
+  print("array",x)
